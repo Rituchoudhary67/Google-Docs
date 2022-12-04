ca461c371d346d4ec2289b86ea102037027a547e
Rituchoudhary67 <ritulalitchoudhary@gmail.com>
2022-12-04 20:39:14 +0530

    second

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const buttons = document.querySelector(".right-nav-btn");
+const fileBtn = document.querySelector(".image");
+const empty = document.querySelector('.empty');
 
 
 /*step1 :- adding event listener on button and make one fun*/
@@ -15,3 +17,24 @@ buttons.addEventListener('mouseout',btnHoverOut);
 function btnHoverOut(e) {
     e.target.style.backgroundColor = "white";
 }
+
+
+// /*step2 :- making fun for filebtn*/
+// function fileOnclick(e){
+//     e.innerHTML = buttons;
+// }
+
+// /* step1 :- add event listener on filebtn and make one call back fun*/
+// fileBtn.addEventListener('click',fileOnclick)
+
+
+fileBtn.addEventListener('click', () => {
+    empty.innerHTML = `<img src="meet.png" alt="">`;
+})
+
+// let one = document.querySelector('.one')
+// let oneImage = document.querySelector('.file-js')
+
+// one.addEventListener('click', () => {
+//     oneImage.innerHTML = `<img src="images/download.jpg" alt="">`
+// })  
